4f020a19baa9d1b7f07bbbd90a70115cc134ce03
darkcoordinate <sagacious112358@gmail.com>
2015-11-04 09:49:36 +0530

    Update vector.c

--- window/vector.c
+++ window/vector.c
@@ -3,11 +3,11 @@
 
 typedef struct
 {
-        float val[4];
+        float *val;
 } Matrix4;
 
 typedef struct{
-        float val[4][4];
+        float **val;
 }Matrix4x4;
 
 Matrix4 Mat_sum(Matrix4 m1, Matrix4 m2)
@@ -36,4 +36,14 @@ Matrix4 Mat_init (float a1, float a2, float a3, float a4);
         Matrix4 ret;
         ret.val = s;
         return ret;
+}
+
+Matrix4x4 Mat4_init(Matrix4 m1, Matrix4 m2, Matrix4 m3, Matrix4 m4)
+{
+        Matrix4x4 ret;
+        ret.val = (float**)malloc(16*sizeof(float));
+        ret.val[0] = m1.val;
+        ret.val[1] = m2.val;
+        ret.val[2] = m3.val;
+        ret.val[3] = m4.val;
 }
